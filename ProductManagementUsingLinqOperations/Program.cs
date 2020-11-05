bd52d52096bea7f859a8fb4e98d530ace4925273
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-05 12:43:24 +0530

    [mmrunal] Add. Added product review list in main method

--- ProductManagementUsingLinqOperations/Program.cs
+++ ProductManagementUsingLinqOperations/Program.cs
@@ -9,7 +9,28 @@ namespace ProductManagementUsingLinqOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management - Linq Operations!");
-            
+            List<ProductReview> reviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1, UserID=1, Rating=5, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=2, UserID=1, Rating=4, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=3, UserID=2, Rating=5, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=4, UserID=2, Rating=4, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=5, UserID=3, Rating=2, Review="Nice", IsLike=false},
+                new ProductReview(){ProductID=6, UserID=3, Rating=5, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=7, UserID=5, Rating=5, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=1, UserID=4, Rating=4, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=11, UserID=6, Rating=2, Review="Nice", IsLike=false},
+                new ProductReview(){ProductID=12, UserID=6, Rating=2, Review="Nice", IsLike=false},
+                new ProductReview(){ProductID=13, UserID=8, Rating=4, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=14, UserID=7, Rating=5, Review="Good", IsLike=true},
+                new ProductReview(){ProductID=15, UserID=9, Rating=1, Review="Bad", IsLike=false},
+                new ProductReview(){ProductID=16, UserID=10, Rating=5, Review="Good", IsLike=true}
+            };
+
+            foreach (var list in reviewList)
+            {
+                Console.WriteLine("Product ID: " + list.ProductID + "\tUser ID: " + list.UserID + "\tRating: " + list.Rating + "\tReview: " + list.Review + "\tIsLike: " + list.IsLike);
+            }
         }
 
     }
