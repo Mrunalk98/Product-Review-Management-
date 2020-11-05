bc7d3578ac60655db630fb8304bb7dc9f803d9fa
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-05 12:14:33 +0530

    [mrunal] Add. Added welcome message in program.cs

---
+++ ProductManagementUsingLinqOperations/Management.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductManagementUsingLinqOperations
+{
+    public class Management
+    {
+    }
+}
